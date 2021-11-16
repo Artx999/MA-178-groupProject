0f0a67e70b37417ebe40868b43bf3c87e1f085e7
Artx999 <54852665+Artx999@users.noreply.github.com>
2021-11-16 18:07:17 +0100

    Hopefully finished Numerical Derivation task 2 d

--- numericalDerivation/task2.py
+++ numericalDerivation/task2.py
@@ -63,7 +63,7 @@ for i in [f_1(), f_2(), f_3(), f_4()]:
     yAxis = []
     for j in xAxis:
         yAxis.append((i.evalf(subs={x: j + dxRounded}) - i.evalf(subs={x: j})) / dxRounded)
-    plt.plot(xAxis, yAxis, label="g_" + str(count) + "(" + str(dxRounded) + ")")
+    plt.plot(xAxis, yAxis, label="g_" + str(count) + "(x)")
 plt.xlabel("x")
 plt.ylabel("y")
 plt.legend(loc="upper right")
@@ -73,9 +73,37 @@ print("Generated plots for task c")
 # Task d
 count = 0
 for i in [f_1(), f_2(), f_3(), f_4()]:
-    print(i)
+    truePath = False
+    nextFunc = False
+    degree = 2
+    count += 1
+    f = 0.001
+    dx = 0.1
+    diff = sym.simplify(sym.diff(i))
+    diffVal = sym.diff(i).evalf(subs={x: x_0(count - 1)})
+    while not nextFunc:
+        g = (i.evalf(subs={x: x_0(count - 1) + dx}) - i.evalf(subs={x: x_0(count - 1)})) / dx
+        E = abs(diffVal - g)
+
+        if E > f:
+            if truePath:
+                dx = dx - 1 / (10 ** (degree + 1))
+                nextFunc = True
+            if not nextFunc:
+                dx = dx / 10
+                degree += 1
+        else:
+            dx = dx + 1 / (10 ** (degree + 1))
+            truePath = True
+    dxRounded = round(dx, degree)
+
+    xAxis = np.linspace(interval(count - 1)[0], interval(count - 1)[1], 100)
+    yAxis = []
+    for j in xAxis:
+        yAxis.append(abs(((i.evalf(subs={x: j + dxRounded}) - i.evalf(subs={x: j})) / dxRounded) - diff.evalf(subs={x: j})))
+    plt.plot(xAxis, yAxis, label="E_" + str(count) + "(x)")
 plt.xlabel("x")
 plt.ylabel("y")
 plt.legend(loc="upper right")
 plt.show()
-print("Generated plots for task c")
+print("Generated plots for task d")
